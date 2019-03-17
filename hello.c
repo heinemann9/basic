a0062d9a2f7fa23c8091ccc3e43f4a0df213871e
ksa <sinankng@gmail.com>
2019-03-17 21:59:56 +0900

    first init

---
+++ hello.c
@@ -0,0 +1,15 @@
+//#include "hello2.h"
+int g = 1;
+extern void hello();
+
+
+int main(void)
+{
+    //hello();
+
+    hello();
+
+    //printf("hello!\n");
+
+    return 0;
+}
---
+++ hello2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int g;
+
+void hello()
+{
+    printf("hello %d !\n", g);
+}
